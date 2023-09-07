f96c146d38d6f60cfd3b1509004ba38572647357
manickmanick <manickaperumal22@gmail.com>
2023-09-07 05:44:11 +0530

    defined user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,21 @@
+// nest g co <name> --no-spec
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
 
 @Controller('user')
 export class UserController {
-  @Get('all/:id')
-  findAll(@Param('id') id: string) {
-    return {
-      user: {
-        id,
-      },
-    };
+  @Get('all')
+  getAll() {
+    return 'all users';
   }
+  // @Get('all/:id')
+  // findAll(@Param('id') id: string) {
+  //   return {
+  //     user: {
+  //       id,
+  //     },
+  //   };
+  // }
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
